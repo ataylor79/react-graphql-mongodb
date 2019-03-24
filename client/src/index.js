1a8364a7789369bde380390a38d29c163a50d906
alec taylor <alec.taylor79@gmail.com>
2019-03-24 21:31:17 +0000

    add simple user auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom'
 import './index.css'
 import App from './components/App'
+import Signin from './components/Auth/Signin'
+import Signup from './components/Auth/Signup'
 
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
@@ -10,9 +18,20 @@ const client = new ApolloClient({
     uri: 'http://localhost:8001/graphql',
 })
 
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/signin" component={Signin} />
+            <Redirect to="/" />
+        </Switch>
+    </Router>
+)
+
 ReactDom.render(
     <ApolloProvider client={client}>
-        <App />
+        <Root />
     </ApolloProvider>,
     document.getElementById('root')
 )
